38a55bb2d6d26b6ba3451188e5030cd9f56a2ca7
tianaFomicheva <tianirika@gmail.com>
2021-01-18 23:10:26 +0500

    totalPrice и totalSum

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -19,14 +19,23 @@ const cart = (state = initialState, action)=>{
                   items: currentPizzaItems,
                   
                 },
-              };          
+              };    
+                    const pizzasCountItemsLength = Object.values(newItems).map(item=>item['items'].length)
+                    const pizzasCountItem = Object.values(newItems).map(item=>item['items'])
+                let startSum = 0
+                const PriceSumObject =      pizzasCountItem.map(item=>item.map(item=>startSum + item.price))
+                
+                const arrForSum = Object.values(Object.values(PriceSumObject))
+                const arrSum = arrForSum.map(item=>item.reduce((count, curNumber)=>count+parseInt(curNumber,0)))
 
         const obj3 = {
             ...state,
             items: newItems,
+            totalCount: pizzasCountItemsLength.reduce((count, curNumber)=>count+curNumber,0),
+            totalPrice: arrSum.reduce((count, curNumber)=>count+curNumber,0),
+
           
         }
-        console.log(obj3)
         return obj3
     }
     
